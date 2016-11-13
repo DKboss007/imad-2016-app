a5d5fd8c8b14b6bd7996bd75a879e4dedc420b4f
dkboss007 <dhruvbhartia425@gmail.com>
2016-11-13 10:05:33 +0530

    Image Gradual Movement

--- ui/main.js
+++ ui/main.js
@@ -6,17 +6,12 @@ element.innerHTML = "New Value!";
 
 //move the image madi
 var img = document.getElementById('madi');
-<<<<<<< HEAD
 var marginLeft = 0;
 function moveRight() {
     marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
 };
+
 img.onclick = function() {
     var interval = setInterval(moveRight, 100);
-}
-=======
-img.onclick = function() {
-    img.style.marginLeft = '100px';
 };
->>>>>>> acb892143f21cf0d9aaaa287a49b2c6b238e94fd
