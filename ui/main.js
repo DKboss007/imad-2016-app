0087fd4ec70020692f6a70177d2f561e18bad635
dkboss007 <dhruvbhartia425@gmail.com>
2016-11-13 09:56:36 +0530

    Image gradual Movement

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,11 @@ element.innerHTML = "New Value!";
 
 //move the image madi
 var img = document.getElementById('madi');
-img.onClick = function() {
-    img.style.marginLeft = '100px';
+var marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
 };
+img.onclick = function() {
+    var interval = setInterval(moveRight, 100);
+}
